bf0a2330bd0f9cec87a0f180d1828cf6fbe665d2
Asepimam <17200931@bsi.ac.id>
2024-06-27 21:44:40 +0700

    refactor: Create FormProfile component for editing user profile information

---
+++ components/FormProfile.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { createClient } from "@/utils/supabase/client";
+import { Button, DatePicker, Form, Input, message } from "antd";
+import dayjs from "dayjs";
+export type FormProfileProps = {
+  first_name: string;
+  last_name: string;
+  full_name: string;
+  birth_date: string;
+};
+
+export default function FormProfile(props: FormProfileProps) {
+  console.log(props);
+  const supabase = createClient();
+  const [form] = Form.useForm();
+  form.setFieldsValue({
+    firstName: props.first_name,
+    lastName: props.last_name,
+    fullName: props.full_name,
+    birthDate: props.birth_date ? dayjs(props.birth_date) : null,
+  });
+  const handleFinish = async (values: any) => {
+    const { firstName, lastName, fullName, birthDate } = values;
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+      const updates = {
+        first_name: firstName,
+        last_name: lastName,
+        full_name: fullName,
+        birth_date: dayjs(birthDate).format("YYYY-MM-DD"),
+      };
+
+      const { error } = await supabase
+        .from("profiles")
+        .update(updates)
+        .eq("user_id", session.user.id);
+
+      if (error) {
+        console.error("Error updating profile:", error);
+        message.error("Error updating profile.");
+      } else {
+        message.success("Profile updated successfully.");
+      }
+    }
+  };
+  return (
+    <div className="flex flex-col w-[600px] rounded-md bg-gray-800 p-16">
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={handleFinish}
+        className="flex flex-col space-y-4">
+        <div className="flex">
+          <div className="flex-1">
+            <Form.Item
+              name="firstName"
+              label={
+                <span className="text-lg font-semibold text-gray-200">
+                  First Name
+                </span>
+              }>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="lastName"
+              label={
+                <span className="text-lg font-semibold text-gray-200">
+                  Last Name
+                </span>
+              }>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="fullName"
+              label={
+                <span className="text-lg font-semibold text-gray-200">
+                  Full Name
+                </span>
+              }>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="birthDate"
+              label={
+                <span className="text-lg font-semibold text-gray-200">
+                  Date of Birth
+                </span>
+              }>
+              <DatePicker />
+            </Form.Item>
+          </div>
+        </div>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Edit Profile
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
--- components/profile.tsx
+++ components/profile.tsx
@@ -1,135 +1,49 @@
-"use client";
-
-import { createClient } from "@/utils/supabase/client";
-import { Button, DatePicker, Form, Input, message } from "antd";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { setUpOIDC } from "@/utils/openid/client";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import FormProfile from "./FormProfile";
 import Skeleton from "./Skeleton";
 
-const supabase = createClient();
+export default async function EditProfile() {
+  const cookie = cookies();
+  const supabase = createClient();
+  const client = await setUpOIDC();
+  let loading = true;
+  let profile = null;
 
-export default function EditProfile() {
-  const [form] = Form.useForm();
-  const [loading, setLoading] = useState(true);
+  const token = cookie.get("token")?.value;
+  console.log(token);
 
-  useEffect(() => {
-    const fetchProfile = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-
-      if (session) {
-        const { data, error } = await supabase
-          .from("profiles")
-          .select("*")
-          .eq("user_id", session.user.id)
-          .single();
-
-        if (error) {
-          console.error("Error fetching profile:", error);
-          message.error("Error fetching profile.");
-        } else {
-          form.setFieldsValue({
-            firstName: data.first_name,
-            lastName: data.last_name,
-            fullName: data.full_name,
-            birthDate: dayjs(data.birth_date),
-          });
-        }
-      }
-      setLoading(false);
-    };
-
-    fetchProfile();
-  }, [form]);
-
-  const handleFinish = async (values: any) => {
-    const { firstName, lastName, fullName, birthDate } = values;
-
+  if (token) {
+    // Get user info using the obtained access token
+    const userinfo = await client.userinfo(token);
+    // Check if user profile already exists in the database
+    const { data: profiles } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("uniq_id", userinfo.sub)
+      .single();
+    profile = profiles;
+    loading = false;
+  }
+  // // jika token tidak ada cek session supabase
+  else {
     const {
       data: { session },
     } = await supabase.auth.getSession();
-
     if (session) {
-      const updates = {
-        first_name: firstName,
-        last_name: lastName,
-        full_name: fullName,
-        birth_date: dayjs(birthDate).format("YYYY-MM-DD"),
-      };
-
-      const { error } = await supabase
+      const { data: profiles } = await supabase
         .from("profiles")
-        .update(updates)
-        .eq("user_id", session.user.id);
-
-      if (error) {
-        console.error("Error updating profile:", error);
-        message.error("Error updating profile.");
-      } else {
-        message.success("Profile updated successfully.");
-      }
+        .select("*")
+        .eq("user_id", session.user.id)
+        .single();
+      profile = profiles;
+      loading = false;
     }
-  };
+  }
 
   if (loading) {
     return <Skeleton />;
   }
-
-  return (
-    <div className="flex flex-col w-[600px] rounded-md bg-gray-800 p-16">
-      <Form
-        form={form}
-        layout="vertical"
-        onFinish={handleFinish}
-        className="flex flex-col space-y-4">
-        <div className="flex">
-          <div className="flex-1">
-            <Form.Item
-              name="firstName"
-              label={
-                <span className="text-lg font-semibold text-gray-200">
-                  First Name
-                </span>
-              }>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name="lastName"
-              label={
-                <span className="text-lg font-semibold text-gray-200">
-                  Last Name
-                </span>
-              }>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name="fullName"
-              label={
-                <span className="text-lg font-semibold text-gray-200">
-                  Full Name
-                </span>
-              }>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name="birthDate"
-              label={
-                <span className="text-lg font-semibold text-gray-200">
-                  Date of Birth
-                </span>
-              }>
-              <DatePicker />
-            </Form.Item>
-          </div>
-        </div>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Edit Profile
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+  return <FormProfile {...profile} />;
 }
