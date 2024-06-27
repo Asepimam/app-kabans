3e96776b1c28902327e3f788e2e9943ce7dd1d1c
Asepimam <17200931@bsi.ac.id>
2024-06-27 21:41:27 +0700

    refactor: Update dashboard page to fetch and display user profile information

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,50 @@
+import { setUpOIDC } from "@/utils/openid/client";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export default async function Dasboard() {
+export default async function Dashboard() {
   const supabase = createClient();
+  let profile = { full_name: "" };
   const {
     data: { user },
+    error: userError,
   } = await supabase.auth.getUser();
-  if (!user) {
-    return redirect("/login");
+  if (user) {
+    const { data } = await supabase
+      .from("profiles")
+      .select("full_name")
+      .eq("user_id", user!.id)
+      .single();
+
+    if (data) {
+      profile = data;
+    }
   }
+
+  if (userError) {
+    const client = await setUpOIDC();
+    const cookie = cookies().get("token")?.value;
+    if (cookie) {
+      const userinfo = await client.userinfo(cookie);
+      const { data } = await supabase
+        .from("profiles")
+        .select("full_name")
+        .eq("uniq_id", userinfo.sub)
+        .single();
+
+      console.log(data);
+
+      if (data) {
+        profile = data;
+      }
+    }
+  }
+
   return (
     <div className="flex justify-center items-center h-screen ">
       <div className="bg-white p-8 rounded shadow-md">
         <h1 className="text-3xl font-bold text-center">Welcome to Dashboard</h1>
-        <p className="text-center text-gray-500 mt-2">{`You are logged in !${user?.email}`}</p>
+        <p className="text-center text-gray-500 mt-2">{`You are logged in! ${profile.full_name}`}</p>
       </div>
     </div>
   );
