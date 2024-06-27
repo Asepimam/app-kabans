f71cacc51357edac4dcec6d23aca125c4381bbce
Asepimam <17200931@bsi.ac.id>
2024-06-27 21:44:15 +0700

    refactor: Update AuthButton to fetch and display user profile information

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,13 +1,36 @@
+import { setUpOIDC } from "@/utils/openid/client";
 import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function AuthButton() {
   const supabase = createClient();
-
+  const cookie = cookies();
+  const client = await setUpOIDC();
+  const token = cookie.get("token")?.value;
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  let user = null;
+
+  if (token) {
+    const userinfo = await client.userinfo(token);
+    const { data: profiles } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("uniq_id", userinfo.sub)
+      .single();
+    user = profiles;
+  } else if (session) {
+    const { data: profiles } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("user_id", session.user.id)
+      .single();
+    user = profiles;
+  }
 
   const signOut = async () => {
     "use server";
@@ -19,12 +42,12 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOut}>
+      👋 Hey, {user.full_name}!
+      <Link href={"/api/logout"}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
         </button>
-      </form>
+      </Link>
     </div>
   ) : (
     <Link
