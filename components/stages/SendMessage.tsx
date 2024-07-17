74b60025eab422968ff2016fa31807f2a399826d
Asepimam <17200931@bsi.ac.id>
2024-07-17 13:21:13 +0700

    refactor: Update FormDetailTask component to improve layout and disable input fields

--- components/stages/SendMessage.tsx
+++ components/stages/SendMessage.tsx
@@ -102,7 +102,7 @@ export default function SendMessage({ task, drawerOpen }: SendMessageProps) {
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
-              stroke-width="2"
+              strokeWidth="2"
               d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z"
             />
           </svg>
