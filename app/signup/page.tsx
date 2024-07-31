23ed0c60ee38bafb9961e71a7b793fd44b7cb7a9
Asepimam <17200931@bsi.ac.id>
2024-07-31 11:23:18 +0700

    refactor: Update login and signup pages

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,9 @@
-import { LoginButtons } from "@/components/LoginForm";
+import { RegisterButton } from "@/components/RegisterForm";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function Login() {
+export default async function SignUp() {
   const supabase = createClient();
   const token = cookies().get("token")?.value;
 
@@ -16,13 +16,13 @@ export default async function Login() {
       <div className="flex justify-center items-center h-screen">
         <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 bg-white rounded-lg shadow-lg p-6">
           <h1 className="text-2xl font-bold text-center text-slate-700">
-            Login
+            Sign Up
           </h1>
-          <LoginButtons />
-          <div className="flex justify-start text-gray-900">
-            Don't have an account ?{"  "}
-            <a href="/signup" className="text-blue-500">
-              signUp
+          <RegisterButton />
+          <div className="flex justify-start text-gray-900 ">
+            you have an account ?{"  "}
+            <a href="/login" className="text-blue-500">
+              Login
             </a>
           </div>
         </div>
